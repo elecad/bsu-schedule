1ff028d21c9a90da9d8343237f771e938ca4ace6
Anton Dachin <relas1234@gmail.com>
2022-03-28 23:28:39 +0300

    Frist commit :)

---
+++ src/components/ThisWelcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <h1>Это будущее Расписание...</h1>
+    <h2>Звучит неоднозначно, но...</h2>
+    <h4>Нужно только подождать :)</h4>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "WelcomePage",
+
+  data: () => ({}),
+};
+</script>
---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <welcome-page></welcome-page>
+</template>
+
+<script>
+import WelcomePage from "../components/ThisWelcome.vue";
+
+export default {
+  name: "Home",
+
+  components: {
+    WelcomePage,
+  },
+};
+</script>
